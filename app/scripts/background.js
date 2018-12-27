587378b6b6c2537f512f0b57b442d5c8f757b999
Dani Ona <25461756+dandansoysauce@users.noreply.github.com>
2018-12-27 20:52:56 +0800

    create search logic

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,39 @@
+console.log(`'Allo 'Allo! Event Page`)
+
+browser.contextMenus.removeAll().then(() => {
+  let gettingBangsArray = browser.storage.local.get('faves')
+  gettingBangsArray.then(item => {
+    if (item.faves && item.faves.length > 0) {
+      item.faves.forEach(fave => {
+        browser.contextMenus.create({
+          id: fave.bang,
+          title: fave.name,
+          contexts: ['selection']
+        })
+      })
+    }
+  })
+})
+
 browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-console.log(`'Allo 'Allo! Event Page`)
+browser.storage.onChanged.addListener(deets => {
+  browser.contextMenus.removeAll().then(() => {
+    deets.faves.newValue.forEach(item => {
+      browser.contextMenus.create({
+        id: item.bang,
+        title: item.name,
+        contexts: ['selection']
+      })
+    })
+  })
+})
+
+browser.contextMenus.onClicked.addListener(data => {
+  browser.search.search({
+    query: `${data.menuItemId} ${data.selectionText.trim()}`,
+    engine: 'DuckDuckGo'
+  })
+})
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,26 +1,98 @@
 console.log('From Options');
+// browser.storage.local.clear()
+let faveBangsArray = []
+let gettingBangsArray = browser.storage.local.get('faves').then(function (arrs) {
+    faveBangsArray = arrs.faves ? arrs.faves : []
+})
+
 function loadJSON(callback) {
-    var bangsText = browser.extension.getURL('resources/bangs.json');
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', bangsText, true);
-    // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function() {
-        console.log(xobj)
-        if (xobj.readyState === 4 && xobj.status === "200") {
-            // Required use of an anonymous callback 
-            // as .open() will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
+    const jsonUrl = 'https://raw.githubusercontent.com/dandansoysauce/duckduckgo-cbang/master/app/resources/bangs.json'
+    fetch(jsonUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(result => {
+            callback(result)
+        })
+}
+
+function appendFaves(faveObj) {
+    var ulBangs = document.getElementById('fave-bangs')
+    let newLi = document.createElement('li')
+    let newLabel = document.createElement('label')
+    let span = document.createElement('span')
+
+    newLi.id = faveObj.id
+    span.textContent = faveObj.name
+    newLabel.appendChild(span)
+    newLi.appendChild(newLabel)
+    ulBangs.appendChild(newLi)
+}
+
+function removeFave(faveObjId) {
+    document.getElementById(faveObjId).remove()
+}
+
+function appendList(bangsData) {
+    var ulBangs = document.getElementById('availables')
+    while (ulBangs.firstChild) {
+        ulBangs.removeChild(ulBangs.firstChild);
+    }
+    bangsData.forEach(element => {
+        let newLi = document.createElement('li')
+        let newLabel = document.createElement('label')
+        let checkbox = document.createElement('input')
+        let span = document.createElement('span')
+
+        let isChecked = faveBangsArray.filter(x => x.bang === element.bang)[0]
+
+        span.textContent = element.name
+        checkbox.id = element.bang
+        checkbox.dataset.bangname = element.name
+        checkbox.type = 'checkbox'
+        checkbox.checked = isChecked ? isChecked.selected : false
+        checkbox.onchange = function (e) {
+            const objFave = {
+                name: e.target.dataset.bangname,
+                bang: e.target.id,
+                selected: e.target.checked,
+                id: e.target.dataset.bangname.replace(' ', '')
+            }
+            
+            if (e.target.checked) {
+                appendFaves(objFave)
+                faveBangsArray.push(objFave)
+            } else {
+                removeFave(objFave.id)
+                faveBangsArray = faveBangsArray.filter(x => x.bang !== e.target.id)
+            }
+
+            browser.storage.local.set({
+                faves: faveBangsArray
+            })
         }
-    };
-    xobj.send(null);
+
+        newLabel.appendChild(checkbox)
+        newLabel.appendChild(span)
+        newLi.appendChild(newLabel)
+        ulBangs.appendChild(newLi)
+    });
 }
 
 function restoreOptions () {
-    loadJSON(function(response) {
-        // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
-        console.log(actual_JSON)
+    loadJSON(function(bangs) {
+        var bangsData = bangs.data;
+        appendList(bangsData)        
+
+        faveBangsArray.forEach(element => {
+            appendFaves(element)
+        })
+
+        var search = document.getElementById('search-input')
+        search.oninput = function (e) {
+            var filteredBangs = bangsData.filter(x => x.name.toLowerCase().includes(e.target.value.toLowerCase()))
+            setTimeout(appendList(filteredBangs), 1000)
+        }
     });
 }
 
